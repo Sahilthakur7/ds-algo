7aee8393b3f7852cf66606ea66bc848d35e1bad1
SahilThakur7 <sahilrd07@gmail.com>
2018-05-30 13:02:57 +0530

    sizeof

---
+++ ch1_sizeof.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    char a;
+    printf("Size of 9.75: %d\n",sizeof(9.75));
+    printf("Size of 'D': %d\n",sizeof('D'));
+    printf("Size of a: %d\n",sizeof(a));
+    printf("Size of 600: %d\n",sizeof(600));
+    return 0;
+}
